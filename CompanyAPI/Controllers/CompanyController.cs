7f7e737a61935d31f7fd59ef5655da72bb40524f
Tim Wildenhues <tim.wildenhues@tobit.software>
2018-09-13 10:00:59 +0200

    Init

---
+++ CompanyAPI/Controllers/CompanyController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyAPI.Controllers
+{
+	[Route("api/company")]
+	[ApiController]
+	public class CompanyController : Controller
+	{
+		// GET api/values
+		[HttpGet("getall")]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/values/5
+		[HttpGet("get/{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/values
+		[HttpPost("insert")]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/values/5
+		[HttpPut("update/{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
+
